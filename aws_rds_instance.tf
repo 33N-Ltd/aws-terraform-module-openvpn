b91690dba3d6063854fb204947d63f6122f05c14
Inih <inih@33n.co.uk>
2023-01-11 16:32:38 +0000

    [CLRDEV-3045] Add temp parameter to allow upgrade

--- aws_rds_instance.tf
+++ aws_rds_instance.tf
@@ -18,6 +18,7 @@ resource "aws_rds_cluster_instance" "db_instance" {
 }
 
 resource "aws_rds_cluster" "db_cluster" {
+  allow_major_version_upgrade = "true"
   cluster_identifier           = var.rds_cluster_identifier
   snapshot_identifier          = var.snapshot_identifier
   engine_version               = var.rds_engine_version
