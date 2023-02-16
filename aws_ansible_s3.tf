4ed6bf60be82dbc6806bf1bf69efb6df0372e2ff
Inih <inih@33n.co.uk>
2023-02-16 11:41:38 +0000

    [CLRDEV-3788] Add etag var

--- aws_ansible_s3.tf
+++ aws_ansible_s3.tf
@@ -2,17 +2,19 @@ resource "aws_s3_bucket_object" "db_ansible_playbook" {
   bucket  = aws_s3_bucket.ssm_ansible_bucket.id
   key     = "ansible/openvpn_db_ansible_playbook.yaml"
   content = data.template_file.db_ansible_playbook.rendered
-  etag = filemd5(data.template_file.db_ansible_playbook.rendered)
+  etag = md5(data.template_file.db_ansible_playbook.rendered)
 }
 
 resource "aws_s3_bucket_object" "ssl_ansible_playbook" {
   bucket  = aws_s3_bucket.ssm_ansible_bucket.id
   key     = "ansible/openvpn_ssl_ansible_playbook.yaml"
   content = data.template_file.ssl_ansible_playbook.rendered
+  etag = md5(data.template_file.ssl_ansible_playbook.rendered)
 }
 
 resource "aws_s3_bucket_object" "update_ansible_playbook" {
   bucket  = aws_s3_bucket.ssm_ansible_bucket.id
   key     = "ansible/openvpn_update_ansible_playbook.yaml"
   content = data.template_file.update_ansible_playbook.rendered
+  etag = md5(data.template_file.update_ansible_playbook.rendered)
 }
